53c191789fb7a3ce604c982f934101ddd8579166
mLeviGen <milkovalexandr2004@gmail.com>
2025-10-31 16:38:03 +0000

    feat: add basic types for utils (number/string)

--- src/demo.ts
+++ src/demo.ts
@@ -1,4 +1,4 @@
 import { add, capitalize } from './index';
 
-console.log('sum(any):', add(2, 3));
-console.log('capitalize(any):', capitalize('hello'));
+console.log('sum(typed):', add(2, 3));
+console.log('capitalize(typed):', capitalize('hello'));
