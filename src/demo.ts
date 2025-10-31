bda999caa12542afb6d2a75612a3d2804e2e8f21
mLeviGen <milkovalexandr2004@gmail.com>
2025-10-31 16:41:31 +0000

    feat: add formatNumber with NumberFormatOptions

--- src/demo.ts
+++ src/demo.ts
@@ -1,4 +1,9 @@
-import { add, capitalize } from './index';
+import { add, capitalize, formatNumber } from './index';
 
 console.log('sum(typed):', add(2, 3));
+
 console.log('capitalize(typed):', capitalize('hello'));
+
+// ПОМИЛКА ТИПІВ: рядок замість числа
+
+console.log('format(ok):', formatNumber(123.456, { precision: 2 }));
